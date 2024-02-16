32232e69a7de1756c32f12d1db9db762b5532a7c
Khai Duc <hzpro1221>
2024-02-16 15:56:12 +0700

    fix condention

--- dataset/argent_detection_dataset/conll04_preprocess.py
+++ dataset/argent_detection_dataset/conll04_preprocess.py
@@ -14,16 +14,16 @@ def conll04_processe(batch_size):
 	# Mở dataset
 	# Điền đường dẫn thư mục chứa dataset
 	with open("/content/conll04_train.json") as f: 
-  		train_data = json.load(f)
+		train_data = json.load(f)
 	# with open("/content/conll04_dev.json") as f:
-  	# 	dev_data = json.load(f)
+	# dev_data = json.load(f)
 	# with open("/content/conll04_test.json") as f:
-  	# 	test_data = json.load(f)
+	# 	test_data = json.load(f)
 
 	# Dataset sẽ có 3 trường:
-	#   - Start và End của từng token sau khi qua Tokenizer, Ids của text qua token
-	#   - Agent mask: Start và End
-	#   - Label mask: [0, 0, 0, 1, 1, 0, 0, ...]
+	#	- Start và End của từng token sau khi qua Tokenizer, Ids của text qua token
+	#	- Agent mask: Start và End
+	#	- Label mask: [0, 0, 0, 1, 1, 0, 0, ...]
 
 	# Load tokenizer
 	tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased") # BERT
@@ -31,18 +31,18 @@ def conll04_processe(batch_size):
 	# Lấy ra Ids của text qua tokenizer, start và end của token
 	dataset_ids = []
 	for document in train_data:
-	  start = 0
-	  end = 0
-	  document_tokens = []
-	  tokenized = tokenizer(document["tokens"], add_special_tokens=False)
-	  for i, token in enumerate(tokenized["input_ids"]):
-	    end = end + len(token)
-	    temp = Token(start=start, end=end, ids=token)
-	    start = start + len(token)
-	    document_tokens.append(temp)
-	  document_length = document_tokens[-1].end
-	  document_processed = Document(document_tokens, document_length)
-	  dataset_ids.append(document_processed)
+		start = 0
+		end = 0
+		document_tokens = []
+		tokenized = tokenizer(document["tokens"], add_special_tokens=False)
+		for i, token in enumerate(tokenized["input_ids"]):
+			end = end + len(token)
+			temp = Token(start=start, end=end, ids=token)
+			start = start + len(token)
+			document_tokens.append(temp)
+		document_length = document_tokens[-1].end
+		document_processed = Document(document_tokens, document_length)
+		dataset_ids.append(document_processed)
 
 	# Build candidate_mask and label_mask
 	# Max len = 5
@@ -51,32 +51,32 @@ def conll04_processe(batch_size):
 	max_len = 5
 	# Generate candidate_mask
 	for i, document in enumerate(train_data):
-	  document_candidates = []
-	  label = []
-	  document_length = dataset_ids[i].length
-	  for length in range(1, max_len+1):
-	    for start in range(0, len(document["tokens"]) - length + 1):
-	      add = 0
-	      candidate_mask = Span_candidate(start, start + length)
-	      # print(f'{start} {start + length} {document["entities"][0]["start"]} {document["entities"][0]["end"]}')
-	      # Check điều kiện để add vào label mask
-	      # Nếu candidate trùng với label trong dataset -> 1
-	      # Nếu candidate không trùng với label trong dataset -> 0
-	      for enity in document["entities"]:
-	        if (start ==  enity["start"]) and ((start + length) ==  enity["end"]):
-	          label.append(1)
-	          add += 1
-	      if (add == 0):
-	        label.append(0)
-	      # print(candidate_mask)
-	      document_candidates.append(candidate_mask)
-	  dataset_candidates.append(document_candidates)
-	  dataset_labels.append(label)
+		document_candidates = []
+		label = []
+		document_length = dataset_ids[i].length
+		for length in range(1, max_len+1):
+			for start in range(0, len(document["tokens"]) - length + 1):
+				add = 0
+				candidate_mask = Span_candidate(start, start + length)
+				# print(f'{start} {start + length} {document["entities"][0]["start"]} {document["entities"][0]["end"]}')
+				# Check điều kiện để add vào label mask
+				# Nếu candidate trùng với label trong dataset -> 1
+				# Nếu candidate không trùng với label trong dataset -> 0
+				for enity in document["entities"]:
+					if (start ==  enity["start"]) and ((start + length) ==  enity["end"]):
+						label.append(1)
+						add += 1
+				if (add == 0):
+					label.append(0)
+				# print(candidate_mask)
+				document_candidates.append(candidate_mask)
+		dataset_candidates.append(document_candidates)
+		dataset_labels.append(label)
 	
 	train_data = Conll04Dataset(dataset_ids=dataset_ids, dataset_candidates=dataset_candidates, dataset_labels=dataset_labels)
 
-  	train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+	train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
   	# dev_dataloader = DataLoader(dev_data, batch_size=batch_size, shuffle=True)
   	# test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-  	return train_dataloader
+	return train_dataloader
